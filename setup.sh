15bb8df68253c3dc2c9fa4968743814350adcae1
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2018-12-21 14:06:03 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# get bin
+# get 8.2 bin
 wget -q -O pentaho-business-analytics.bin https://www.hitachivantara.com/bin/services/s3files?filePath=pentaho-business-analytics-8.2.0.0-342-x64.bin
 chmod a+x pentaho-business-analytics.bin
 
