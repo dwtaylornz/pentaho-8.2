c9b435647b82b6d0f3d537af79385f5bfd95816f
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2018-12-21 14:05:28 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # get bin
-wget https://www.hitachivantara.com/bin/services/s3files?filePath=pentaho-business-analytics-8.2.0.0-342-x64.bin
-chmod a+x pentaho-business-analytics-8.2.0.0-342-x64.bin
+wget -q -O pentaho-business-analytics.bin https://www.hitachivantara.com/bin/services/s3files?filePath=pentaho-business-analytics-8.2.0.0-342-x64.bin
+chmod a+x pentaho-business-analytics.bin
 
 # need answer file for installation 
-/pentaho-business-analytics-8.2.0.0-342-x64.bin --mode text
+./pentaho-business-analytics.bin --mode text
